200c91f71d58a0efb5b7bcde1ee03e35fa77d5e3
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-07-09 22:21:28 +0700

    Comment out theme configuration in main.dart for temporary removal of dark theme in Flutter application

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:fairsplit/core/theme/theme.dart';
+// import 'package:fairsplit/core/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -26,7 +26,7 @@ class MyApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       title: 'FairSplit',
       routerConfig: router,
-      theme: AppTheme.darkThemeMode,
+      // theme: AppTheme.darkThemeMode,
     );
   }
 }
