ca24b6e4324c0bb50a21ae176a4c420071accba6
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-07-09 22:21:39 +0700

    Update base URL in ApiConstants to use localhost for local development

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,3 +1,4 @@
 class ApiConstants {
-  static const String baseUrl = 'https://fairsplit-server.onrender.com/api/v1';
+  // static const String baseUrl = 'https://fairsplit-server.onrender.com/api/v1';
+  static const String baseUrl = 'http://localhost:8080/api/v1';
 }
