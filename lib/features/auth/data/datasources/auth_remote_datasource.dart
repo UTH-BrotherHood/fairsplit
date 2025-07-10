b2dff3ec64212d9f115c0a249937677aa5301c2d
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-07-11 04:47:49 +0700

    feat: Implement Google Sign-In functionality in AuthRemoteDataSource and AuthRepository
    - Added signInWithGoogle method to AuthRemoteDataSource for handling Google authentication.
    - Integrated Google sign-in process, including token retrieval and backend communication.
    - Updated AuthRepository to save tokens upon successful Google sign-in, enhancing user authentication experience.

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -7,10 +7,18 @@ import 'package:fairsplit/features/auth/data/models/register_response_model.dart
 import 'package:fairsplit/features/auth/data/models/error_response_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthRemoteDataSource {
   final http.Client client;
   AuthRemoteDataSource(this.client);
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: ['email'],
+    clientId:
+        '639401568459-kki393tuvvagi964s96fg13a7fhscmra.apps.googleusercontent.com', // Nếu cần chỉ định
+  );
 
   Future<AuthResponseModel> login(String email, String password) async {
     final response = await client.post(
@@ -87,7 +95,45 @@ class AuthRemoteDataSource {
     );
   }
 
-  Future signInWithGoogle() async {}
+  Future<AuthResponseModel?> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) return null; // User hủy đăng nhập
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final String? idToken = googleAuth.idToken;
+
+      if (idToken == null) throw Exception('No idToken');
+
+      // Gửi idToken lên backend
+      final response = await http.post(
+        Uri.parse('${ApiConstants.baseUrl}/auth/login/google'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'idToken': idToken}),
+      );
+
+      print("authRes 116s => $response");
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final authResponse = AuthResponseModel.fromJson(data);
+
+        print("authRes 122 => $authResponse");
+
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString('accessToken', authResponse.data.accessToken);
+        await prefs.setString('refreshToken', authResponse.data.refreshToken);
+
+        return authResponse;
+      } else {
+        throw Exception('Backend error: ${response.body}');
+      }
+    } catch (e) {
+      print('Google Sign-In error: $e');
+      return null;
+    }
+  }
 
   Future<AuthResponseModel> refreshToken(String refreshToken) async {
     final response = await client.post(
